3d0ce933bee147ed5bf81d0279afc270049d0ad8
robotbird <robotbird@qq.com>
2020-02-21 00:35:43 +0800

    doc:commit规范文档改成大写,新增appCategory测试类

---
+++ src/test/java/com/webtap/respository/AppCategoryRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.webtap.respository;
+
+import com.webtap.domain.AppCategory;
+import com.webtap.repository.AppCategoryRepository;
+import com.webtap.repository.AppRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppCategoryRepositoryTest {
+
+	@Autowired
+  private AppCategoryRepository appCategoryRepository;
+
+	@Test
+	public void testUpdateAppAmount() throws Exception {
+           appCategoryRepository.updateAppAmount(1L,-2L);
+	}
+
+}
