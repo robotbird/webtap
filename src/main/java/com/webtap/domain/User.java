7c6d6c011a935460b2388eeb17dda0f6c9a150e0
叶鹏 <robotbird@qq.com>
2020-04-19 21:43:10 +0800

    fix:没有图片的时候显示首字的样式

--- src/main/java/com/webtap/domain/User.java
+++ src/main/java/com/webtap/domain/User.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "wt_users")
-public class User extends Entitys implements Serializable {
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
